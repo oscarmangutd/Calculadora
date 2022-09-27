75521d0b517dc11de3f2181f71136bf2c99b2f7a
oscarmangutd <oscarmangutd91@gmail.com>
2022-09-27 14:55:36 +0200

    Actualizacion obteneryearActual

--- obtenerYearActual.js
+++ obtenerYearActual.js
@@ -1,4 +1,9 @@
-let yearActual;
+/*let yearActual;
 const moonLanding = new Date();
 
 console.log(moonLanding.getFullYear());
+*/
+let yearActual;
+const obtenerYear = new Date();
+
+console.log(obtenerYear.getFullYear());
